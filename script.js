d6c7af1350f4ef017685ef32d2fcda17755c0423
Elliot <gurgithemastermind@gmail.com>
2020-03-19 20:21:29 -0400

    Finished the animation!

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 window.onload = function what() {
   var Boat = document.getElementById('Boat').style;
+  var Background = document.getElementById('Background');
+
+  var cloudArray = [
+    document.querySelector("body > div > div.clouds > .cloud:nth-child(1)"),
+    document.querySelector("body > div > div.clouds > .cloud:nth-child(2)"),
+    document.querySelector("body > div > div.clouds > .cloud:nth-child(3)"),
+    document.querySelector("body > div > div.clouds > .cloud:nth-child(4)"),
+  ];
+
+  var wave = document.querySelector("body > div > div.waves");
+
+  console.log("Width: " + Background.offsetWidth);
 
   var amp = 12/2;
   var freq = 1/32;
@@ -8,15 +20,58 @@ window.onload = function what() {
 
   var angleAmp = 2;
 
-  var id = setInterval(frame, 5);
+  var boatId = setInterval(boatAnim, 5);
+  var cloudId = setInterval(cloudAnim, 30);
+  var waveId = setInterval(waveAnim, 20);
 
-  function frame() {
+  var spawnWidth = 3000;
+  var randomness = 40;
+  var cloudHeight = 100;
+  var cloudHeightRand = 300;
+
+  var cloudCoors = [[0,0],[0,0],[0,0],[0,0]];
+
+  cloudArray.forEach((cloud, i) => {
+    cloudCoors[i] = [(Math.ceil(Math.random() * randomness)+ spawnWidth*i/4),(Math.ceil(Math.random() *cloudHeightRand)-cloudHeight)]
+    cloud.style.transform = "scale(0.5)"+ " translate(" + cloudCoors[i][0] + "px," + cloudCoors[i][1] + "px)";
+  });
+
+
+
+  function cloudAnim() {
+    cloudArray.forEach((cloud, i) => {
+      //if it hits the end reset the various things
+      if (cloudCoors[i][0]>2200) {
+        var cloudNum = Math.ceil(Math.random() * 5);
+        cloudCoors[i][0] = -800;
+        cloudCoors[i][1] = (Math.ceil(Math.random() *cloudHeightRand)-cloudHeight);
+      }
+      cloudCoors[i][0] += 1;
+      cloud.style.transform = "scale(0.5)"+ " translate(" + cloudCoors[i][0] + "px," + cloudCoors[i][1] + "px)";
+    });
+
+  }
+
+  var waveX = 0;
+  //wave the entire wave snaps back exactly on top of itself so the movement looks seemless
+  var breakPoint = 197;
+
+  function waveAnim() {
+    if (waveX == breakPoint) {
+      waveX = 1;
+    }else {
+      waveX++;
+    }
+
+    wave.style.transform = "translateX(" + waveX + "px)";
+
+  }
+
+  function boatAnim() {
     position = amp*Math.sin(i*freq*Math.PI*2);
     angle = angleAmp*Math.atan(-amp*freq*Math.PI*2*Math.cos(i*freq*Math.PI*2));
     Boat.transform = "translateY(" + position + "px) " + "rotate(" + angle + "deg)";
-    console.log(position);
 
     i += 0.05;
-
   }
 }

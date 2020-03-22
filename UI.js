e90c87e468c5ad235d57c9d1332389e8d70c82ae
Elliot <gurgithemastermind@gmail.com>
2020-03-22 13:29:47 -0400

    Everything's finished except for the shop

--- UI.js
+++ UI.js
@@ -1,37 +1,7 @@
 window.onload = function what() {
-  var Boat = document.getElementById('Boat').style;
-  var Background = document.getElementById('Background');
-  var backgroundMusic = document.getElementsByClassName('backgroundMusic');
-  var ambientMusic = document.getElementsByClassName('ambientMusic');
-  var upgradeSFX = document.getElementById('upgradeSFX');
-  var playButton = document.getElementById('playButton');
-  var startScreen = document.getElementById('startScreen');
-  var currentMusic = 0;
-  playButton.onclick = function(){
-    startScreen.classList.add("slideAnimation");
-    backgroundMusic[currentMusic].volume = 0.1;
-    backgroundMusic[currentMusic].play();
-    ambientMusic[0].volume = 0.1;
-    ambientMusic[0].play();
-
-  };
-
-  var cloudArray = [
-    document.querySelector("body > div > div.clouds > .cloud:nth-child(1)"),
-    document.querySelector("body > div > div.clouds > .cloud:nth-child(2)"),
-    document.querySelector("body > div > div.clouds > .cloud:nth-child(3)"),
-    document.querySelector("body > div > div.clouds > .cloud:nth-child(4)"),
-  ];
-
-  var wave = document.querySelector("body > div > div.waves");
-
-  var mast = document.getElementsByClassName('Mast');
-  //console.log(mast);
-  var hull = document.getElementsByClassName('Hull');
-  //console.log(hull);
-  var cannon = document.getElementsByClassName('Cannon');
-
   var costTag = document.getElementsByClassName('costTag')[0];
+  var timeTag = document.getElementById('timeTag');
+  var timeText = document.getElementById('timeText')
   var costText = document.getElementById('costText');
 
   var hullButton = document.getElementById('hullButton');
@@ -84,87 +54,20 @@ window.onload = function what() {
     open = !open;
   });
 
+  /*var bottomButton = document.getElementById('bottomButton');
+  bottomButton.addEventListener("click",buyBottomItem);
+  bottomButton.addEventListener("mousemove",function showCost(event) {
+    var x = event.clientX;
+    var y = event.clientY;
+    timeTag.style.opacity = 1;
+    timeTag.style.transform = "translate("+(x+30)+"px,"+(y-30)+"px)";
+    costText.innerHTML = bottomCost;
+    currentItemTime = "bottom";
+  });
+  setInterval(updateShop,1000);*/
+
   var currentFlag = 0;
   var flag = document.getElementsByClassName('Flag')[currentFlag];
-
-  console.log("Width: " + Background.offsetWidth);
-
-  var amp = 12/2;
-  var freq = 1/32;
-  var position;
-  var i = 0;
-
-  var angleAmp = 2;
-
-  var boatId = setInterval(boatAnim, 5);
-  var cloudId = setInterval(cloudAnim, 40);
-  var waveId = setInterval(waveAnim, 20);
-  var riseId;
-
-  var spawnWidth = 3000;
-  var randomness = 40;
-  var cloudHeight = 100;
-  var cloudHeightRand = 300;
-
-  var cloudCoors = [[0,0],[0,0],[0,0],[0,0]];
-
-  cloudArray.forEach((cloud, i) => {
-    cloudCoors[i] = [(Math.ceil(Math.random() * randomness)+ spawnWidth*i/4),(Math.ceil(Math.random() *cloudHeightRand)-cloudHeight)]
-    cloud.style.transform = "scale(0.5)"+ " translate(" + cloudCoors[i][0] + "px," + cloudCoors[i][1] + "px)";
-  });
-
-
-//Animations
-  function cloudAnim() {
-    cloudArray.forEach((cloud, i) => {
-      //if it hits the end reset the various things
-      if (cloudCoors[i][0]>2200) {
-        var cloudNum = Math.ceil(Math.random() * 5);
-        cloudCoors[i][0] = -800;
-        cloudCoors[i][1] = (Math.ceil(Math.random() *cloudHeightRand)-cloudHeight);
-      }
-      cloudCoors[i][0] += 1;
-      cloud.style.transform = "scale(0.5)"+ " translate(" + cloudCoors[i][0] + "px," + cloudCoors[i][1] + "px)";
-    });
-
-  }
-
-  var waveX = 0;
-  //wave the entire wave snaps back exactly on top of itself so the movement looks seemless
-  var breakPoint = 197;
-
-  function waveAnim() {
-    if (waveX == breakPoint) {
-      waveX = 1;
-    }else {
-      waveX++;
-    }
-
-    wave.style.transform = "translateX(" + waveX + "px)";
-
-  }
-
-  function boatAnim() {
-    position = amp*Math.sin(i*freq*Math.PI*2);
-    angle = angleAmp*Math.atan(-amp*freq*Math.PI*2*Math.cos(i*freq*Math.PI*2));
-    Boat.transform = "translateY(" + position + "px) " + "rotate(" + angle + "deg)";
-
-    i += 0.05;
-  }
-
-  var j;
-  var riseHeight;
-
-  function riseAnim() {
-    if (++j>riseHeight) {
-      clearInterval(riseId);
-      j = 0;
-    } else {
-      Boat.top = 111 - j - riseHeight*(currentHull-2) + "px";
-    }
-  }
-
-  //upgrade functions
 
   var currentHull = 1;
   var currentMast = 1;
